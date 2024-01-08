daa5e76573ce1e4706d8c3a0e4dcd4db99fa69d2
svitlana petryk <svitlanaa.petryk@gmail.com>
2024-01-08 11:44:23 +0100

    hw5

---
+++ src/hw5.ts
@@ -0,0 +1,45 @@
+abstract class Shape {
+  constructor(public readonly name: string, public readonly color: string) {}
+
+  abstract calculateArea(): number;
+}
+class Circle extends Shape {
+  constructor(private radius: number, color: string) {
+    super('Circle', color);
+  }
+
+  calculateArea(): number {
+    return Math.PI * this.radius ** 2;
+  }
+}
+class Rectangle extends Shape {
+  constructor(private height: number, private width: number, color: string) {
+    super('Rectangle', color);
+  }
+  calculateArea(): number {
+    return this.height * this.width;
+  }
+  print(): string {
+    return 'S = width * height';
+  }
+}
+class Square extends Shape {
+  constructor(private side: number, color: string) {
+    super('Square', color);
+  }
+  calculateArea(): number {
+    return this.side ** 2;
+  }
+  print(): string {
+    return 'S = side * side';
+  }
+}
+class Triangle extends Shape {
+  constructor(private baseSide: number, private height: number, color: string) {
+    super('Triangle', color);
+  }
+   calculateArea(): number {
+    return (this.baseSide * this.height) / 2;
+  }
+}
+
